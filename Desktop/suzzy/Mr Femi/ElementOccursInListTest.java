31ee06f35ad49cbb8d6288fc26472345f37025a7
Olubiyi Oluwaseyi Susannah <105393206+susannaholubiyi@users.noreply.github.com>
2023-12-19 15:32:27 +0100

    Update ElementOccursInListTest.java

--- Desktop/suzzy/Mr Femi/ElementOccursInListTest.java
+++ Desktop/suzzy/Mr Femi/ElementOccursInListTest.java
@@ -1,15 +1,57 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class ElementOccursInListTest{
 
 	@Test
-	public void testThatElementOccursInAList(){
+	public void testThatIntegerOccursInAList(){
 	
 	ElementOccursInList element = new ElementOccursInList();
 	
 	int[] numbers = {1, 2, 3, 4, 5, 6};
-	int numberToBeCompared = element.compareNumber(newNumber);
+	int number = 9;
+	boolean numberToBeCompared = element.compareNumber(numbers, number);
+	
+	assertEquals(false, numberToBeCompared);
 	
 	
+	}
+	
+	@Test
+	public void testThatStringOccursInAList(){
+	
+	ElementOccursInList element = new ElementOccursInList();
+	
+	String[] words = {"i", "am", "a", "girl"};
+	String word = "girl";
+	
+	boolean wordToBeCompared = element.compareWord(words, word);
+	assertEquals(true, wordToBeCompared);
+	
+	
+	}
+	
+	@Test
+	public void testThatDoubleOccursInAList(){
+	
+	ElementOccursInList element = new ElementOccursInList();
+	
+	double[] decimals = {1.1, 2.2, 3.3, 4.4, 5.5};
+	double decimal = 6.6;
+	
+	boolean decimalToBeCompared = element.compareDecimal(decimals, decimal);
+	assertEquals(false, decimalToBeCompared);
+	}
+	
+	@Test
+	public void testThatCharOccursInAList(){
+	
+	ElementOccursInList element = new ElementOccursInList();
+	
+	char[] letters = {'a', 'e', 'i', 'o', 'u'};
+	char letter = 'e';
+	
+	boolean charToBeCompared = element.compareChar(letters, letter);
+	assertEquals(true, charToBeCompared);
 	}
 
 
