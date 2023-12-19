235bbe8a6ebee48b330d186bb2ca0628f2a9ce03
Olubiyi Oluwaseyi Susannah <105393206+susannaholubiyi@users.noreply.github.com>
2023-12-19 15:29:19 +0100

    new commit

---
+++ Desktop/suzzy/Mr Femi/ElementOccursInListTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+public class ElementOccursInListTest{
+
+	@Test
+	public void testThatElementOccursInAList(){
+	
+	ElementOccursInList element = new ElementOccursInList();
+	
+	int[] numbers = {1, 2, 3, 4, 5, 6};
+	int numberToBeCompared = element.compareNumber(newNumber);
+	
+	
+	}
+
+
+
+
+}
