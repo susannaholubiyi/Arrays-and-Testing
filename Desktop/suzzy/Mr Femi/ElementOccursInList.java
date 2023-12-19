6664e2546069670fd29a0d07ca2188a506e359cb
Olubiyi Oluwaseyi Susannah <105393206+susannaholubiyi@users.noreply.github.com>
2023-12-19 15:31:23 +0100

    Update ElementOccursInList.java

--- Desktop/suzzy/Mr Femi/ElementOccursInList.java
+++ Desktop/suzzy/Mr Femi/ElementOccursInList.java
@@ -1,7 +1,69 @@
 public class ElementOccursInList{
 
 
-	public int compareNumber (int newNUmber);
-	int count = 0;
+	public boolean compareNumber (int[] numbers, int element){
+	int index = 0;
 	
+		while(index < numbers.length){
+		
+
+	
+			if(element == numbers[index]){
+				
+				return true;
+				
+	
+			}
+
+	
+			
+			index ++;
+	
+		}
+		return false;
+	
+	}
+	
+	
+	public boolean compareWord (String[] words, String word){
+	
+		for(int index = 0; index < words.length; index ++){
+	
+			if (word.equals(words[index])){
+			return true;
+			
+		}
+
+		}
+	
+	return false;
+	
+	
+	}
+	
+	public boolean compareDecimal (double[] decimals, double decimal){
+	
+		for(int index = 0; index <decimals.length; index++){
+		
+			if(decimal == decimals[index]){
+			return true;
+			}
+		
+		}
+	
+	return false;
+	}
+	
+	
+	public boolean compareChar (char[] letters, char letter){
+	
+		for(int index = 0; index < letters.length; index++){
+		
+			if(letter == letters[index]){
+			return true;
+			}
+		}
+		
+	return false;
+	}
 }
